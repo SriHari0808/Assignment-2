ecac1f31b07bcaa819939b021a40d1056027945f
SriHari0808 <sganeshbabu0808@conestogac.on.ca>
2022-09-23 09:14:42 -0400

    Updated the code to read StudentData_Emails file if compiled using PRE_RELEASE directive

--- Assignment 2/Source.cpp
+++ Assignment 2/Source.cpp
@@ -1,16 +1,19 @@
-
 #include<iostream>
 #include<fstream>
 #include<string>
 #include<vector>
 #include<sstream>
-#define PRE_RELEASE
 using namespace std;
+#define PRE_RELEASE
 
-struct STUDENT_DATA  //Created a struct STUDENT_DATA that contains the first and last names of the students.
+struct STUDENT_DATA                 //Created a struct STUDENT_DATA that contains the first and last names of the students.
 {
 	string fname;
 	string lname;
+
+#ifdef PRE_RELEASE
+	string email;
+#endif
 };
 
 int main()
@@ -24,33 +27,47 @@ int main()
 	ifstream fin;
 	vector<STUDENT_DATA> student_vector;
 	STUDENT_DATA stud;
-	fin.open("StudentData.txt");              //Opening the file
+	string filename;
+
+#ifdef PRE_RELEASE                                    
+	filename = "StudentData_Emails.txt";
+#else
+	filename = "StudentData.txt";
+#endif // PRE_RELEASE
+
+	fin.open(filename);									 //Opening the initilized filename 
 	if (fin.is_open())
 	{
 		while (!fin.eof())
 		{
 			string str;
-			getline(fin, str);                //Reading the lines from the file
+			getline(fin, str);                          //Reading the lines from the file
 
 			istringstream ss(str);
 			string fname, lname;
-			getline(ss, stud.lname, ',');       //Parsing the data
-			getline(ss, stud.fname);
+			getline(ss, stud.lname, ',');               //Parsing the data
+			getline(ss, stud.fname, ',');
 
-			student_vector.push_back(stud);     //Pushing the data to vector
+#ifdef PRE_RELEASE
+			getline(ss, stud.email);
+#endif
+			student_vector.push_back(stud);             //Pushing the data to vector
 		}
 		fin.close();
 	}
 	else
 	{
-		cout << "Oops! Cannot open StudentData file" << endl;
+		cout << "Oops! Cannot open " << filename << endl;
 	}
 
 #ifdef _DEBUG
-	for (int i = 0; i < student_vector.size(); i++)  //Printing the student data only in debug mode
+	for (int i = 0; i < student_vector.size(); i++)      //Printing the student data only in debug mode
 	{
+#ifdef PRE_RELEASE
+		cout << student_vector[i].fname << " " << student_vector[i].lname << " " << student_vector[i].email << endl;
+#else
 		cout << student_vector[i].fname << " " << student_vector[i].lname << endl;
+#endif
 	}
 #endif // DEBUG
-	return 0;
 }
